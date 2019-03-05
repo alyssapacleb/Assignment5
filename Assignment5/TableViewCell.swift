4298e837d729174ce3159858dc0ab032e9de50bc
Alyssa June Pacleb <alyssapacleb@alyssas-macbook-pro.local>
2019-03-04 23:42:24 -0600

    Some cells

---
+++ Assignment5/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Assignment5
+//
+//  Created by Alyssa June Pacleb on 3/4/19.
+//  Copyright © 2019 Alyssa June Pacleb. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
