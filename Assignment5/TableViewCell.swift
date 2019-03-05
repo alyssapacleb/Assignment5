6fc15994fd6848fda838e72bfa52907857b0c8b8
Alyssa June Pacleb <alyssapacleb@alyssas-macbook-pro.local>
2019-03-05 00:14:28 -0600

    Some interface done

--- Assignment5/TableViewCell.swift
+++ Assignment5/TableViewCell.swift
@@ -13,6 +13,11 @@ class TableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        //let cellIdentifier = “CustomTableViewCell"
+        //let cell = tableView.dequeueReusableCell(withIdentifier:
+        //    cellIdentifier, for: indexPath) as! CustomTableViewCell
+        //cell.cellLabel.text = content[indexPath.row]
+        //return cell
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
